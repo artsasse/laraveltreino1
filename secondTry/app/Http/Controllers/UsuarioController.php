2dc7fbd2ce49e792202576b8ef274837198650ec
artsasse <30945018+artsasse@users.noreply.github.com>
2018-01-27 19:39:59 -0200

    not_the_first

---
+++ secondTry/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Usuario;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $firstname = $request->input('firstname');
+      $lastname = $request-> input('lastname');
+      /*
+      $usuario= new  Usuario;
+      $usuario->nome=$firstname;
+      $usuario->sobrenome=$lastname;
+      $usuario->save();*/
+
+      Usuario::create(['nome' => $firstname,
+                     'sobrenome'=>$lastname,
+      ]);
+    }
+
+    public function inicial()
+        {
+            //gerencia dados aqui.
+
+            return view('welcome');
+        }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ secondTry/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+*/
+
+/*Route::get('/silk', 'UsuarioController@inicial')->name('SilkGetter');
+Route::post('/silk', 'UsuarioController@store')->name('SilkRoad');*/
+
+Route::get('contato', 'ContatoController@create')->name('contato.create');
+Route::post('contato', 'ContatoController@store')->name('contato.store');
